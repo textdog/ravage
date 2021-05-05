2aa4caae8dce89312defa8c93a1f481e6097cf28
tying <tycheng@gmail.com>
2021-05-04 22:25:48 -0400

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -9,7 +9,7 @@ export default function About ({ data }) {
             <h1>
                About {data.site.siteMetadata.title}. 
             </h1>
-            <p>Ravage is a 5 year project orchestrated by Tiff Gum Ying Cheng. Many of the scenes in it depended on a mood of nervous triumph, especially since the script was handwritten and put into some order using tape and was therefore almost of no help.
+            <p>Ravage is a 7 year project orchestrated by Tiff Gum Ying Cheng. Many of the scenes in it depended on a mood of nervous triumph, especially since the script was handwritten and put into some order using tape and was therefore almost of no help.
             </p>
             <p>Email us: <a href="mailto:porkornopork@gmail.com">porkornopork  at   g-popular email service  dot com.</a></p>
         </div>
