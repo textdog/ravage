4cc9d9c526f79138c49453c98e85876027e3cde2
bnstelle <88566078+bnstelle@users.noreply.github.com>
2021-09-18 14:51:54 -0400

    Update index.js
    quickly added mailchimp signup embed

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,6 +21,28 @@ export default function Home({ data }) {
         </h1>
         <img src="https://i.vimeocdn.com/video/845098068_620.jpg" alt="/"></img>
         <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
+
+        <!-- Begin Mailchimp Signup Form -->
+<link href="//cdn-images.mailchimp.com/embedcode/horizontal-slim-10_7.css" rel="stylesheet" type="text/css">
+<style type="text/css">
+  #mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; width:100%;}
+  /* Add your own Mailchimp form style overrides in your site stylesheet or in this style block.
+     We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+</style>
+<div id="mc_embed_signup">
+<form action="https://ravagecity.us5.list-manage.com/subscribe/post?u=d6fa36d42324513c637a142da&amp;id=83dedd8fba" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+    <div id="mc_embed_signup_scroll">
+  
+  <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_d6fa36d42324513c637a142da_83dedd8fba" tabindex="-1" value=""></div>
+    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+    </div>
+</form>
+</div>
+
+<!--End mc_embed_signup-->
+
         {data.allMarkdownRemark.edges.map(({ node }) => (
           <div key={node.id}>
             <Link 
