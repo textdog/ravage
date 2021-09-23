953f60319d71d9d8fd6f3883fd9358a27d9c9203
bnstelle <88566078+bnstelle@users.noreply.github.com>
2021-09-23 19:54:57 -0400

    mailchimp code update on gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,10 +12,11 @@ module.exports = {
   },
   plugins: [
       {
-  resolve: 'gatsby-plugin-mailchimp',
-  options: {
-    endpoint: 'https://ravagecity.us5.list-manage.com/subscribe/post?u=d6fa36d42324513c637a142da&amp;id=83dedd8fba'
-      },
+        resolve: 'gatsby-plugin-mailchimp',
+        options: {
+            endpoint: 'https://ravagecity.us5.list-manage.com/subscribe/post?u=d6fa36d42324513c637a142da&amp;id=83dedd8fba', // string; add your MC list endpoint here; see instructions below
+            timeout: 3500, // number; the amount of time, in milliseconds, that you want to allow mailchimp to respond to your request before timing out. defaults to 3500
+        },
     },
       {
         resolve: `gatsby-source-filesystem`,
