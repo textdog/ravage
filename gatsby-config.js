de23ececcd03aca7b3e895efa1e8f3f580d0882a
himanshu-fullstackdev <73710597+himanshu-fullstackdev@users.noreply.github.com>
2022-01-22 15:24:21 +0530

    updated the config file

--- gatsby-config.js
+++ gatsby-config.js
@@ -42,5 +42,13 @@ module.exports = {
     },
   `gatsby-plugin-offline`,
   `gatsby-plugin-react-helmet`,
+    {
+      resolve: "gatsby-plugin-mailchimp",
+      options: {
+        endpoint:
+          "https://ravagecity.us5.list-manage.com/subscribe/post?u=d6fa36d42324513c637a142da&id=83dedd8fba", // string; add your MC list endpoint here; see instructions below
+        timeout: 3500, // number; the amount of time, in milliseconds, that you want to allow mailchimp to respond to your request before timing out. defaults to 3500
+      },
+    },
   ]
 }
