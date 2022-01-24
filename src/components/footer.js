7c2a8d5165ff310e6deccd9e6fecae9c5f66ffb8
himanshu-fullstackdev <73710597+himanshu-fullstackdev@users.noreply.github.com>
2022-01-24 20:25:24 +0530

    updated the form

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,50 +4,66 @@ import footerStyles from "./footer.module.css"
 import ArrowRight from "../images/arrow-right.svg"
 
 const Footer = () => {
+  const [email, setEmail] = useState("")
+  const [isSubmitting, setIsSubmitting] = useState(false)
+  const [message, setMessage] = useState("")
+  const [showSuccess, setShowSuccess] = useState(false)
+  const [showFailure, setShowFailure] = useState(false)
 
-  const [email, setEmail]                 = useState("");
-  const [isSubmitting, setIsSubmitting]   = useState(false);
-  const [message, setMessage]             = useState("");
-  const [showSuccess, setShowSuccess]     = useState(false);
-  const [showFailure, setShowFailure]     = useState(false);
-
-  const _handleChange = (e) => {
-    setEmail(e.target.value);
+  const _handleChange = e => {
+    setEmail(e.target.value)
   }
 
-  const _handleSubmit = async (e) => {
-    e.preventDefault();
-    setIsSubmitting(true);
-    setShowSuccess(false);
-    setShowFailure(false);
-    setMessage("");
-    const result = await addToMailchimp(email);
-    console.log(result);
-    setIsSubmitting(false);
-    setMessage(result.msg);
+  const _handleSubmit = async e => {
+    e.preventDefault()
+    setIsSubmitting(true)
+    setShowSuccess(false)
+    setShowFailure(false)
+    setMessage("")
+    const result = await addToMailchimp(email)
+    console.log(result)
+    setIsSubmitting(false)
+    setMessage(result.msg)
     if (result.result === "success") {
-      setShowSuccess(true);
-      setEmail("");
+      setShowSuccess(true)
+      setEmail("")
     } else if (result.result === "error") {
-      setShowFailure(true);
+      setShowFailure(true)
     }
   }
 
   return (
     <footer>
-      <form className={footerStyles.mailChimpForm}>
+      <h2 className={footerStyles.mailChimpTitle}>Sign up for newsletter</h2>
+      <form
+        className={footerStyles.mailChimpForm}
+        onSubmit={e => email && _handleSubmit(e)}
+      >
         <input
           type="email"
           name="email"
-          placeholder="Sign up for email address"
-          onChange={(e) => _handleChange(e)}
+          placeholder="Email address"
+          onChange={e => _handleChange(e)}
           value={email}
           className={footerStyles.mailChimpInput}
         />
-        <img onClick={(e) => email && _handleSubmit(e)} className={[footerStyles.mailChimpSubmit, isSubmitting || !email ? footerStyles.mailChimpDisabled : ''].join(' ')} src={ArrowRight} />
+        <button
+          type="submit"
+          className={footerStyles.mailChimpSubmit}
+          disabled={isSubmitting || !email}
+        >
+          Sign up
+        </button>
       </form>
-      {showSuccess ? <div className={footerStyles.footerSuccessMessage} >{message}</div> : null}
-      {showFailure ? <div className={footerStyles.footerErrorMessage} dangerouslySetInnerHTML={{__html: message}}></div> : null}
+      {showSuccess ? (
+        <div className={footerStyles.footerSuccessMessage}>{message}</div>
+      ) : null}
+      {showFailure ? (
+        <div
+          className={footerStyles.footerErrorMessage}
+          dangerouslySetInnerHTML={{ __html: message }}
+        ></div>
+      ) : null}
     </footer>
   )
 }
