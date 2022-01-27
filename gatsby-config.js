30bd63f7baa33e0c81cf7687f3127c57b7cd69be
bnstelle <88566078+bnstelle@users.noreply.github.com>
2022-01-27 11:38:27 -0500

    removed .env and replaced with string
    Something about the .env replacement of the mailchimp endpoint string was breaking the signup form. I removed the .env part for now and replaced with the simple string as recorded in the plugin docs.

--- gatsby-config.js
+++ gatsby-config.js
@@ -49,7 +49,7 @@ module.exports = {
     {
       resolve: "gatsby-plugin-mailchimp",
       options: {
-        endpoint: process.env.MAILCHIMP_KEY, // string; add your MC list endpoint here; see instructions below
+        endpoint: 'https://ravagecity.us5.list-manage.com/subscribe/post?u=d6fa36d42324513c637a142da&amp;id=83dedd8fba', // string; add your MC list endpoint here; see instructions below
         timeout: 3500, // number; the amount of time, in milliseconds, that you want to allow mailchimp to respond to your request before timing out. defaults to 3500
       },
     },
